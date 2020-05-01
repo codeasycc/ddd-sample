642cd40256d71fade1b489b0ef3a305ec0e4c1cc
codeasycc <codeasycc@126.com>
2020-05-01 18:00:30 +0800

    init

---
+++ credit/src/main/java/cc/codeasy/ddd/sample/mall/credit/CreditApplication.java
@@ -0,0 +1,13 @@
+package cc.codeasy.ddd.sample.mall.credit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditApplication.class, args);
+	}
+
+}
---
+++ credit/src/test/java/cc/codeasy/ddd/sample/mall/credit/CreditApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.codeasy.ddd.sample.mall.credit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
