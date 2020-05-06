9f64907097987b4f5a94c867c819ffce9fcbc6db
codeasycc <codeasycc@126.com>
2020-05-06 15:04:14 +0800

    增加使用ApplicationEventPublisher的领域事件发布器

--- credit/src/main/java/cc/codeasy/ddd/sample/mall/credit/common/DomainEventPublisher.java
+++ credit/src/main/java/cc/codeasy/ddd/sample/mall/credit/common/DomainEventPublisher.java
@@ -1,9 +1,18 @@
 package cc.codeasy.ddd.sample.mall.credit.common;
 
 import cc.codeasy.ddd.sample.mall.credit.domain.model.RechargeRequestAcceptedEvent;
+import org.springframework.context.ApplicationEventPublisher;
 
 public class DomainEventPublisher {
+
+    private final ApplicationEventPublisher publisher;
+
+
+    public DomainEventPublisher(ApplicationEventPublisher publisher) {
+        this.publisher = publisher;
+    }
+
     public void publish(Object event) {
-        //使用Spring对Event机制实现
+        publisher.publishEvent(event);
     }
 }
